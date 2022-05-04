d46f9f8a0af141391728a02fcbfb3097f2c2f3bd
bpalotai <88626268+bpalotai@users.noreply.github.com>
2022-05-04 11:05:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,15 +14,8 @@ def favicon():
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
-
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+def hello(name_value):
+   return {'name': name_value, 'res': 'Siker!!'}
 
 
 if __name__ == '__main__':
